d8648d49d5e8293e25a04d726265280974d91004
dcoderath <trivedi2693@gmail.com>
2025-05-09 22:39:25 +0530

    Allow custom host in Vite config and update related files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['59zkmx-5173.csb.app'], // Add the domain you need to allow
+  },
 });
